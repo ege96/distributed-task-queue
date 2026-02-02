2a71c2765d404cb977c8f8cadcd0274e63489e1f
davidli9410 <david.li9410@gmail.com>
2026-02-02 14:44:41 -0500

    singleton with connection pooling

--- src/distributed_task_queue/storage/client.py
+++ src/distributed_task_queue/storage/client.py
@@ -2,13 +2,26 @@ import redis
 from distributed_task_queue.config.settings import load_settings
 
 
+class RedisFactory:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(RedisFactory,cls).__new__(cls)
+            cls._instance._initialize()
+        return cls._instance
+
+
+    def _initialize(self):
+        settings = load_settings()
+
+        self.pool = redis.ConnectionPool(host=settings.redis_host,port = settings.redis_port, db = settings.redis_db)
+    def get_client(self):
+        return redis.Redis(connection_pool=self.pool)
+    
+
+
 
 def redis_client():
-    settings = load_settings()
-
-    host = settings.redis_host
-    port = settings.redis_port
-    db = settings.redis_db
+    return RedisFactory().get_client()
     
-    client = redis.Redis(host=host, port=port, db=db)
-    return client
